f1613028898121af2bafe465a36bd4c2601d222e
mkoohafkan <michael.koohafkan@gmail.com>
2015-01-25 23:38:09 -0800

    updated OFAC implementation so outputs are consistent.

--- R/reval.r
+++ R/reval.r
@@ -1,43 +1,11 @@
-
-# eval_permutate = function(fun, pargs, default.args, sample=TRUE, sample.size){}
-# collate_perumate = function(l, reshape.fun, id.name){}
-
-eval_ofac = function(fun, pargs, default.args){
-  # setup inputs
-  for(n in names(pargs))
-    if(is.null(names(pargs[[n]])))
-      names(pargs[[n]]) = paste(pargs[[n]])
-  # setup outputs
-  ret = vector("list", length=length(pargs))
-  names(ret) = names(pargs)
-  for(n in names(ret)){
-    ret[[n]] = vector("list", length=length(pargs[[n]]))
-    names(ret[[n]]) = names(pargs[[n]])
-  }
-  # evaluate
-  for(n in names(ret))
-    for(v in names(ret[[n]])){
-      args = default.args
-      args[[n]] = pargs[[n]][[v]]
-      tryCatch({
-        ret[[n]][[v]] = do.call(fun, args)
-      }, error=function(cond) {
-        message("When evaluating ", n, " = ", v, ": ", cond)
-      }, warning=function(cond) {
-        message("When evaluating ", n, " = ", v, ": ", cond)
-      })
-    }
-  return(ret)  
-}
-
 eval_set = function(fun, pargs, default.args){
-  expand.arguments <- function(...){
+  recycle.grid <- function(...){
     dotList <- list(...)
     max.length <- max(sapply(dotList, length))
     lapply(dotList, rep, length=max.length)
   }
   # setup inputs
-  setpargs = do.call(expand.arguments, pargs) # recycle values for sets  
+  setpargs = do.call(recycle.grid, pargs) # recycle values for sets  
   for(n in names(setpargs))
     if(is.null(names(setpargs[[n]])))
       names(setpargs[[n]]) = paste(setpargs[[n]])
@@ -71,7 +39,18 @@ eval_set = function(fun, pargs, default.args){
   return(ret)
 }
 
-collate_set = function(l, reshape.fun, id.name){
+eval_permute = function(fun, pargs, default.args, size){
+  arg.grid = expand.grid(pargs)
+  if(is.na(size))
+    size = nrow(arg.grid)  
+  permargs = as.list(arg.grid[sample(seq(nrow(arg.grid)), size = size),])
+  attributes(permargs) <- NULL 
+  names(permargs) = names(arg.grid)
+  eval_set(fun, permargs, default.args)
+}
+
+collate_set = function(l, reshape.fun, collate.id){
+  id.name = "set"
   res = NULL
   for(n in names(l))
     if(!is.null(l[[n]])){
@@ -79,44 +58,64 @@ collate_set = function(l, reshape.fun, id.name){
       nres[id.name] = n
       res = rbind(res, nres)
     }
-  res
+  if(collate.id == "single")
+    res
+  else
+    parse_set(res, id.name)
 }
 
-collate_ofac = function(l, reshape.fun, id.name){
-  res = NULL
-  for(n in names(l)){
-    nres = NULL
-    for(v in names(l[[n]])){
-      if(!is.null(l[[n]][[v]])){
-        vres = as.data.frame(reshape.fun(l[[n]][[v]]))
-        vres[id.name] = paste(n, "=", v)
-        nres = rbind(nres, vres)
-      }
-    }
-    res = rbind(res, nres)
+eval_ofac = function(fun, pargs, default.args){
+  base.args = lapply(pargs, function(x) x[1])
+  ret = list()
+  for(p in names(pargs)){
+    thisargs = base.args
+    thisargs[[p]] = pargs[[p]]
+    ret = append(ret, eval_set(fun, thisargs, default.args))
   }
-  return(res)
+  return(ret)
 }
 
-#' Repeated evaluations along one dimension
+parse_set = function(df, n){
+  x = df[[n]]
+  l = NULL
+  variables = strsplit(x, " ; ") 
+  for(i in seq(length(variables))){
+    value = strsplit(variables[[i]], " = ")
+    v = sapply(value, function(x) x[2])
+    names(v) = sapply(value, function(x) x[1])
+    l = rbind(l, v)
+  }
+  rownames(l) = NULL
+  out = cbind(df, l)
+  out[[n]] = NULL
+  out
+}
+
+
+#' Repeated evaluations
 #'
-#' Evaluate a function repeatedly, varying one argument at a time.
+#' Evaluate a function repeatedly across argument sets or permutations.
 #'
 #' @param fun The function to be evaluated.
 #' @param ... Arguments to be varied when evaluating \code{fun}, where each 
-#'   argument in \code{...} is a vector or list of values.
-#' @param method The sensitivity analysis method to be used. Currently supports 
-#'   one-factor-at-a-time ("ofac") evaluation and evaluation of parameter sets
-#'   ("set").
+#'   argument in \code{...} is a vector or list of values. 
+#' @param method The sensitivity analysis method to be used. Can be either 
+#'   one-factor-at-a-time ("ofac") evaluation, evaluation of parameter sets
+#'   ("set"), or (sampled) permutations of parameter sets ("permute"). When
+#'   \code{method = "ofac"}, the first element of each argument in \code{...}
+#'   is assumed to be the "default" value of that argument.
+#' @param sample If \code{method = "permute"}, the number of parameter 
+#'   permutations to evaluate. If NA, all permutations will be evaluated.
 #' @param default.args the default values of arguments passed to \code{fun}.
-#'   for \code{method = "ofac"}, must include a default value for each argument
-#'   in \code{...}.
 #' @param collate Whether to collate the results or not. If TRUE, output 
 #'   elements will be coerced into data.frames using \code{as.data.frame}.
 #' @param reshape.fun A function for reshaping the output of each evaluation
 #'   prior to coercing and collating the outputs. 
-#' @param id.name The name of the column used to store the evaluation 
-#'   identifiers. Defaults to the value of \code{method}.
+#' @param collate.id Only used when \code{collate = TRUE}. The method used to 
+#'   store the evaluation identifiers. If \code{collate.id = "single"}, a single 
+#'   column column is used. If \code{collate.id = "multi"}, one column is created 
+#'   for each argument in \code{...}. \code{collate.id = "multi"} can at times 
+#'   simplify data exploration and visualization.
 #'
 #' @return If \code{collate = TRUE}, a data.frame. Otherwise, a list structure 
 #'   depending on the value of \code{method}:
@@ -128,17 +127,22 @@ collate_ofac = function(l, reshape.fun, id.name){
 #'   \item{"set"}{A simple list.}
 #'
 #' @examples
+#' set.seed(1459)
 #' eval_many(rnorm, mean = c(5, 9), sd = c(2, 3), default.args = list(n = 5))
-#' eval_many(rnorm, mean = c(5, 9), sd = c(2, 3), reshape.fun = t, 
-#'   default.args = list(n = 5, mean = 0, sd = 5))
-#' # vector recyclying
+#' eval_many(rnorm, mean = c(5, 9), sd = c(2, 3), default.args = list(n = 5),
+#'   reshape.fun = t, collate.id = "multi")
+#' eval_many(rnorm, mean=seq(20), sd = seq(1, 4, by = 0.1), 
+#'  default.args = list(n = 5), method = "permute", sample = 10,
+#'   reshape.fun = t)
+#'
+#' # vector recycling
 #' eval_many(rnorm, mean = c(0, 5, 3), sd = c(0, 1), method = "set", 
-#'   default.args = list(n = 5), reshape.fun = t, id.name = "set")
+#'   default.args = list(n = 5), reshape.fun = t, collate.id = "multi")
 #' 
 #' \donttest{\dontrun{
 #' formulas = list(y ~ x, y ~ x^2, y ~ x + z)   
 #' datasets = list(
-#'   A = data.frame(x=1:10, y = 1:10 + rlnorm(10)),
+#'   A = data.frame(x=1:10, y = 1:10 + rnorm(10)),
 #'   B = data.frame(x=1:10, y = 1:10 + rnorm(10, mean = 2))
 #' )
 #'
@@ -147,33 +151,25 @@ collate_ofac = function(l, reshape.fun, id.name){
 #'   collate = FALSE)
 #'
 #' # data extraction
-#' eval_many(lm, formula = formulas, data = datasets[1], method = "set", 
-#'   reshape.fun = function(x) t(x$coefficients))
-#' eval_many(lm, formula = formulas, data = datasets, method = "ofac", 
-#'   default.args = list(formula = formulas[[1]], data = datasets[[1]]), 
-#'   reshape.fun = function(x) t(x$coefficients))
+#' eval_many(lm, formula = formulas, data = datasets, method = "permute",
+#'   reshape.fun = function(x) t(x$coefficients), collate.id = "multi")
 #' }}
 #' @export
-eval_many = function(fun, ..., method = "ofac", default.args = list(), 
-  collate = TRUE, reshape.fun = identity, id.name){
-  method = match.arg(method, c("ofac", "set"))
-  if(missing(id.name))
-    id.name = method
+eval_many = function(fun, ..., method = c("ofac", "permute", "set"), 
+  sample = NA, default.args = list(), collate = TRUE, 
+  reshape.fun = identity, collate.id = c("single", "multi")){
+  method = match.arg(method, c("ofac", "permute", "set"))
+  collate.id = match.arg(collate.id, c("single", "multi"))
   pargs = list(...)
   if(method == "ofac")
     res = eval_ofac(fun, pargs, default.args)
   else if(method == "set")
     res = eval_set(fun, pargs, default.args)
   else
-    stop("how did you get here...?")
+    res = eval_permute(fun, pargs, default.args, size = sample)
   # format outputs
   if(collate == FALSE)
     res
   else
-    if(method == "ofac")
-      collate_ofac(res, reshape.fun, id.name)
-    else if(method == "set")
-      collate_set(res, reshape.fun, id.name)
-    else
-      stop("you shouldn't be here...")
+    collate_set(res, reshape.fun, collate.id)
 }
