1abab58068af81223f94b7b650abf5f036e8f00e
mkoohafkan <michael.koohafkan@gmail.com>
2015-03-27 19:37:02 -0700

    improvements to parallel processing and collate ids to resolve issues #1, #2, #3

--- R/reval.r
+++ R/reval.r
@@ -1,5 +1,13 @@
+#' This package provides the function \code{evalmany} to simplify scenario 
+#' testing and sensitivity analysis with R.
+#'
+#' @name reval-package
+#' @docType package
+#' @import foreach parallel doParallel
+NULL
 
-eval_set = function(fun, pargs, default.args){
+
+eval_set = function(fun, pargs, default.args, pkg){
   recycle.grid = function(...){
     dotList = list(...)
     max.length = max(sapply(dotList, length))
@@ -36,7 +44,7 @@ eval_set = function(fun, pargs, default.args){
   an = NULL # stupidity for CRAN
   as = NULL
   ret = foreach(as = argsets, an = setnames, .errorhandling = "pass", 
-    .inorder = FALSE, .combine = 'c') %dopar% {
+    .inorder = FALSE, .combine = 'c', .packages = pkg) %dopar% {
     tryCatch({
       structure(list(do.call(fun, as)), names = an)
     }, error = function(cond){
@@ -55,29 +63,29 @@ eval_set = function(fun, pargs, default.args){
   return(ret)
 }
 
-eval_permute = function(fun, pargs, default.args, size){
+eval_permute = function(fun, pargs, default.args, size, pkg){
   arg.grid = expand.grid(pargs)
   if(size < 1)
     size = nrow(arg.grid)  
   permargs = as.list(arg.grid[sample(seq(nrow(arg.grid)), size = size),])
   attributes(permargs) = NULL 
   names(permargs) = names(arg.grid)
-  eval_set(fun, permargs, default.args)
+  eval_set(fun, permargs, default.args, pkg)
 }
 
-eval_ofat = function(fun, pargs, default.args){
+eval_ofat = function(fun, pargs, default.args, pkg){
   base.args = lapply(pargs, function(x) x[1])
   ret = list()
   for(p in names(pargs)){
     thisargs = base.args
     thisargs[[p]] = pargs[[p]]
-    ret = append(ret, eval_set(fun, thisargs, default.args))
+    ret = append(ret, eval_set(fun, thisargs, default.args, pkg))
   }
   return(ret)
 }
 
 collate_set = function(l, collate.fun, collate.id){
-  id.name = "reval.set"
+  id.name = "eval.id"
   res = NULL
   for(n in names(l))
     if(!is.null(l[[n]])){
@@ -98,7 +106,7 @@ parse_set = function(df, n){
   for(i in seq(length(variables))){
     value = strsplit(variables[[i]], " = ")
     v = sapply(value, function(x) x[2])
-    names(v) = sapply(value, function(x) x[1])
+    names(v) = paste0("eval.", sapply(value, function(x) x[1]))
     l = rbind(l, v)
   }
   rownames(l) = NULL
@@ -110,8 +118,6 @@ parse_set = function(df, n){
 #' Repeated evaluations
 #'
 #' Evaluate a function repeatedly across argument sets or permutations.
-#'
-#' @import foreach parallel doParallel
 #'
 #' @param fun The function to be evaluated.
 #' @param ... Arguments to be varied when evaluating \code{fun}, where each 
@@ -133,27 +139,28 @@ parse_set = function(df, n){
 #'   Otherwise, the raw outputs will be returned as a named list.
 #' @param collate.id If \code{collate = TRUE}, the method used to 
 #'   store the evaluation identifiers. If \code{collate.id = "single"}, a 
-#'   single column is used. If \code{collate.id = "multi"}, one column is 
-#'   created for each argument in '\code{...}'. 
+#'   single column named 'eval.id' is used. If \code{collate.id = "multi"}, 
+#'   one column is created for each argument in '\code{...}', e.g. 
+#'   'eval.arg1', 'eval.arg2', etc. 
 #' @param collate.fun If \code{collate = TRUE}, an optional function 
 #'   for reshaping the output of each evaluation prior to coercing and 
 #'   collating the outputs. 
 #' @param clusters Number of clusters to use for parallel processing. Default
 #'   is 1 (serial computation).
+#' @param packages For parallel processing. Character vector of packages that 
+#'   'fun' depends on.
 #'
 #' @return If \code{collate = TRUE}, a data.frame. Otherwise, a named list.
 #'
 #' @examples
-#' set.seed(1459)
-#' myfun = function(...){ 
-#'   x = rnorm(...) 
+#' myfun = function(n, mean=0, sd = 1){ 
+#'   x = rnorm(n, mean, sd) 
 #'   data.frame(mean = mean(x), sd = sd(x))
 #' }
 #' evalmany(myfun, mean = c(5, 9), sd = c(2, 3), default.args = list(n = 1e6))
-#' evalmany(myfun, mean = c(5, 9), sd = c(2, 3), default.args = list(n = 1e6),
-#'   collate.id = "multi")
 #' evalmany(myfun, mean = seq(20), sd = seq(1, 4, by = 0.1), 
-#'   default.args = list(n = 1e6), method = "permute", sample = 10)
+#'   default.args = list(n = 1e6), method = "permute", sample = 10,
+#'   collate.id = "multi")
 #'
 #' # vector recycling
 #' evalmany(myfun, mean = c(0, 3, 5), sd = c(1, 10), 
@@ -165,17 +172,18 @@ parse_set = function(df, n){
 #'   clusters = 2)
 #'
 #' \dontrun{
-#' # Complex objects and error handling
-#' formulas = list(y ~ 1, y ~ x, y ~ I(x^2), y ~ x + z)   
+#' # Complex objects 
+#' formulas = list(y ~ 1, y ~ x, y ~ x + z)   
 #' datasets = list(
 #'   A = data.frame(x = seq(0, 99), y = seq(0, 99) + rnorm(100)),
 #'   B = data.frame(x = seq(0, 99), y = seq(0, 99) + rnorm(100, mean = 5)),
-#'   C = data.frame(x = seq(0, 99), y = seq(0, 99) + rlnorm(100, meanlog = 1))
+#'   C = data.frame(x = seq(0, 99), y = seq(0, 99) + rlnorm(100, meanlog = 1),
+#'     z = seq(0, 99) - rlnorm(100, meanlog = -1))
 #' )
 #' # raw output
 #' evalmany(lm, formula = formulas, data = datasets, method = "set", 
 #'   collate = FALSE)
-#' # data extraction
+#' # data extraction and error handling
 #' evalmany(lm, formula = formulas, data = datasets, method = "permute",
 #'   collate.id = "multi", collate.fun = function(x) 
 #'     data.frame(param = names(x$coefficients), value = x$coefficients, 
@@ -186,7 +194,8 @@ parse_set = function(df, n){
 evalmany = function(fun, ..., method = c("ofat", "permute", "set"), 
  sample = 0L, default.args = list(), collate = TRUE, 
  collate.id = c("single", "multi"), collate.fun = identity, 
- clusters = 1L){
+ clusters = 1L, packages = NULL){
+  # argument checks
   if(any(names(list(...)) == ""))
     stop("All arguments to 'fun' must be named")
   method = match.arg(method, c("ofat", "permute", "set"))
@@ -196,19 +205,21 @@ evalmany = function(fun, ..., method = c("ofat", "permute", "set"),
   if(any(sapply(pargs, function(x) any(grepl(";", names(x))))))
     stop('argument names must not contain ";".')
   # prepare for parallel processing
-  if(clusters < 0)
-    stop('argument "clusters" must be positive.')
-  cl <- makeCluster(clusters)
-  registerDoParallel(cl)
+  if(clusters > 1){
+    cl <- makeCluster(clusters)
+    on.exit(stopCluster(cl))
+    registerDoParallel(cl)
+  }
+  else
+    registerDoSEQ()
   # process
   if(method == "ofat")
-    res = eval_ofat(fun, pargs, default.args)
+    res = eval_ofat(fun, pargs, default.args, packages)
   else if(method == "set")
-    res = eval_set(fun, pargs, default.args)
+    res = eval_set(fun, pargs, default.args, packages)
   else
-    res = eval_permute(fun, pargs, default.args, size = as.integer(sample))
-  # cleanup
-  stopCluster(cl)
+    res = eval_permute(fun, pargs, default.args, size = as.integer(sample), 
+      packages)
   # format outputs
   if(collate == FALSE)
     res
